786e41bbd2cf2b08efa7707290d6477f92c3d7ed
mahesh <kagithacm237@gmail.com>
2025-10-05 22:34:38 +0530

    first sections

--- src/App.js
+++ src/App.js
@@ -1,66 +1,99 @@
-import { Box, Grid, Paper, TextField } from "@mui/material";
+import { Box, Grid, Paper, TextField, Typography } from "@mui/material";
 import React from "react";
 
 export default function App() {
   return (
     <>
-      <Paper
-        elevation={4}
-        sx={{
-          p: 2,
-          height: "100%", // Ensures Paper fills the vertical space if content were aligned
-          borderRadius: 2,
-          bgcolor: "#ffffff",
-        }}
-      >
-        <Grid container spacing={2} mt={2}>
-          <Grid size={{ xs: 6, md: 4 }}>
-            <GridRow label="CCD">
-              <Box sx={{ py: 0.5, px: 1, bgcolor: "#e0e0e0", borderRadius: 1 }}>
-                CCDValue
-              </Box>
-            </GridRow>
-            <GridRow label="TEO">
-              <Box sx={{ py: 0.5, px: 1, bgcolor: "#e0e0e0", borderRadius: 1 }}>
-                TEOValue
-              </Box>
-            </GridRow>
-            <GridRow label="Associated TEO">
-              <Box sx={{ py: 0.5, px: 1,  borderRadius: 1 }}>
-              <TextField size="small" ></TextField>
-              </Box>
-            </GridRow>
-             <GridRow label="Associated CCR">
-              <Box sx={{ py: 0.5, px: 1,  borderRadius: 1 }}>
-                <TextField size="small" ></TextField>
-              </Box>
-            </GridRow>
+      <div>
+        <Paper
+          elevation={4}
+          sx={{
+            p: 2,
+            height: "100%", // Ensures Paper fills the vertical space if content were aligned
+            borderRadius: 2,
+            bgcolor: "#ffffff",
+          }}
+        >
+          <Typography
+            variant="h5"
+
+          >
+            E1 Tracking
+            <hr/>
+          </Typography>
+          <Grid container spacing={2} mt={2}>
+            {/* <Grid
+            size={{ xs: 12, md: 12 }}
+            sx={{
+              fontWeight: "bold",
+              display: "flex",
+              alignItems: "center",
+              paddingTop: "0px !important",
+            }}
+          >
+            <Box sx={{  borderRadius: 1 }}>E1 Tracking</Box>
+
+          </Grid> */}
+            <Grid size={{ xs: 6, md: 4 }}>
+              <GridRow label="E1R#:">
+                <Box sx={{ borderRadius: 1 }}>NEW</Box>
+              </GridRow>
+              <GridRow label="CCR:">
+                <Box sx={{ borderRadius: 1 }}>N/A</Box>
+              </GridRow>
+              <GridRow label="TEO:">
+                <Box sx={{ borderRadius: 1 }}>TEOValue</Box>
+              </GridRow>
+              <GridRow label="Associated TEO:">
+                <Box sx={{ py: 0.5, px: 1, borderRadius: 1 }}>
+                  <TextField size="small"></TextField>
+                </Box>
+              </GridRow>
+              <GridRow label="Associated CCR:">
+                <Box sx={{ py: 0.5, px: 1, borderRadius: 1 }}>
+                  <TextField size="small"></TextField>
+                </Box>
+              </GridRow>
+            </Grid>
+            <Grid size={{ xs: 6, md: 4 }}>
+              <GridRow label="E1 Tracking Status:">
+                <Box sx={{ borderRadius: 1 }}>New</Box>
+              </GridRow>
+              <GridRow label="Assignment Input Status:">
+                <Box sx={{ borderRadius: 1 }}>N/A</Box>
+              </GridRow>
+              <GridRow label="Detailed Engineering Complete:">
+                <Box sx={{ borderRadius: 1 }}>N/A</Box>
+              </GridRow>
+              <GridRow label="Build Mile Stone:">
+                <Box sx={{ borderRadius: 1 }}>N/A</Box>
+              </GridRow>
+              <GridRow label="In-Effect MileStone:">
+                <Box sx={{ borderRadius: 1 }}>N/A</Box>
+              </GridRow>
+            </Grid>
+            <Grid size={{ xs: 6, md: 4 }}>
+              <GridRow label="Expedite:">
+                <Box sx={{ borderRadius: 1 }}>
+                  <input type="checkbox"></input>
+                </Box>
+              </GridRow>
+              <GridRow label="Notify NMA">
+                <Box sx={{ borderRadius: 1 }}>
+                  <input type="checkbox"></input>
+                </Box>
+              </GridRow>
+            </Grid>
           </Grid>
-          <Grid size={{ xs: 6, md: 4 }}>
-            <GridRow label="CCD">
-              <Box sx={{ py: 0.5, px: 1, bgcolor: "#e0e0e0", borderRadius: 1 }}>
-                CCDValue
-              </Box>
-            </GridRow>
+          <Grid container spacing={2} mt={2}>
+            <Grid size={{ xs: 6, md: 4 }}>
+              <GridRow label="CCD">
+                <Box sx={{ borderRadius: 1 }}>CCDValue</Box>
+              </GridRow>
+            </Grid>
           </Grid>
-          <Grid size={{ xs: 6, md: 4 }}>
-            <GridRow label="CCD">
-              <Box sx={{ py: 0.5, px: 1, bgcolor: "#e0e0e0", borderRadius: 1 }}>
-                CCDValue
-              </Box>
-            </GridRow>
-          </Grid>
-        </Grid>
-        <Grid container spacing={2} mt={2}>
-          <Grid size={{ xs: 6, md: 4 }}>
-            <GridRow label="CCD">
-              <Box sx={{ py: 0.5, px: 1, bgcolor: "#e0e0e0", borderRadius: 1 }}>
-                CCDValue
-              </Box>
-            </GridRow>
-          </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
+      </div>
     </>
   );
 }
